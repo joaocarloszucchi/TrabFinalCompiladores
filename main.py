e1691de4402d9d99b69f354931d097072aff284d
joaocarloszucchi <jczucchi@inf.ufsm.br>
2024-07-09 14:16:19 -0300

    added in-time input

--- main.py
+++ main.py
@@ -10,4 +10,11 @@ if __name__ == "__main__":
     grammar = Grammar('grammar_exp.json')
     grammar.print()
     grammar.validate_grammar()
-    grammar.validate_sentence("int + int * int / int - int * int")
+
+    keep = True
+    while keep:
+        word = input("Type a sentence for validation(or exit to leave): ")
+        if word == "exit":
+            keep = False
+        else:
+            grammar.validate_sentence(str(word))
